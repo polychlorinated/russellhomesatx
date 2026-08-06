6d8fbed8a71cb57951ff4cf6f3a454725d288ce2
polychlorinated <andrew@polychlorinated.com>
2026-08-05 21:15:55 -0500

    initial commit

---
+++ src/data/site.ts
@@ -0,0 +1,44 @@
+export const site = {
+  name: "Sara + Kevin Russell | Austin Homes",
+  tagline: "Diligently Guiding You Home",
+  brokerage: "Holt Residential Group",
+  brokerageUrl: "https://holtresidential.com/",
+  broker: "Compass",
+};
+
+export const sara = {
+  firstName: "Sara",
+  name: "Sara Russell",
+  title: "REALTOR\u00ae",
+  phone: "(512) 826-6312",
+  phoneHref: "tel:+15128266312",
+  email: "sararussell@compass.com",
+  compassUrl: "https://www.compass.com/agents/sara-russell-tx",
+  headshot: "/sara-headshot.webp",
+};
+
+export const kevin = {
+  firstName: "Kevin",
+  name: "Kevin Russell",
+  title: "REALTOR\u00ae",
+  phone: "(512) 460-0668",
+  phoneHref: "tel:+15124600668",
+  compassUrl: "https://www.compass.com/agents/kevin-russell",
+  headshot: "/kevin-headshot-new.webp",
+};
+
+export const links = {
+  instagram: "https://www.instagram.com/russellhomesatx/",
+  linktree: "https://linktr.ee/russellhomesatx512",
+  compassSearch: "https://www.compass.com/homes-for-sale/austin-tx/",
+  trecIabs: "/Sara%20Russell%20IABS.pdf",
+  kevinIabs: "/Kevin%20Russell%20IABS.pdf",
+  trecConsumerNotice: "/TREC%20Consumer%20Protection%20Notice.pdf",
+};
+
+export const nav = [
+  { label: "Home", href: "/" },
+  { label: "Meet Sara + Kevin", href: "/about" },
+  { label: "Neighborhoods", href: "/neighborhoods" },
+  { label: "Home Search", href: links.compassSearch, external: true },
+];
